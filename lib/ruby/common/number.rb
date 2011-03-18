1a4c603d30fd947a3e17244e5756e5b99936b0be
Ryan Gantt <ryan@zuttonet.com>
2011-03-18 14:22:06 -0600

    added collatz(n) and commented

--- lib/ruby/common/number.rb
+++ lib/ruby/common/number.rb
@@ -1,3 +1,4 @@
+# find the smallest number that divides every number between first and last
 def sed( first, last )
     n = last
     while n += 1
@@ -10,7 +11,19 @@ def sed( first, last )
     end
 end
 
+# return the factorial of integer n
 def fact(n)
    (2..n-1).each {|i| n*= i}
    n
+end
+
+# find the length of the collatz chain for integer n
+def collatz(n)
+	len = 0
+	while n > 1
+		n = 3*n + 1 if n%2 != 0
+		n = n/2 if n%2 == 0
+		len += 1
+	end
+	return len
 end
