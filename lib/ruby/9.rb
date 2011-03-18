37dc9a212e74dc43f87a543e17ae18db4e2644c1
Ryan Gantt <ryan@zuttonet.com>
2011-03-18 12:29:07 -0600

    formatting

--- lib/ruby/9.rb
+++ lib/ruby/9.rb
@@ -2,10 +2,10 @@
 
 max = 1000
 (1..max).each do |i|
-    (i..(max-i)).each do |j|
+	(i..(max-i)).each do |j|
 		c = max - i - j
 		if ( i**2 + j**2 ) == c**2
 			puts i, j, c, i*j*c
-        end
-    end
+		end
+	end
 end
