d237c39b7d5e42bea8e9dbed42494e28d093da36
Ryan Gantt <ryan@zuttonet.com>
2011-03-18 16:10:25 -0600

    commentation

--- lib/ruby/common/prime.rb
+++ lib/ruby/common/prime.rb
@@ -1,3 +1,4 @@
+# returns the largest prime factor for n
 def lpf( n )
 	i = 2
 	while i < n
@@ -7,21 +8,23 @@ def lpf( n )
 	n
 end
 
+# returns true if n is prime, nil otherwise
 def prime?( n )
 	return nil if n == 1
 	return true if n < 4
 	return nil if n % 2 == 0
 	return true if n < 9
 	return nil if n % 3 == 0
-    (5..Math.sqrt(n).floor).step(6) do |i|
-        if n % i == 0 or n % ( i + 2 ) == 0
-            return nil
-        end
-    end
-    true
+	(5..Math.sqrt(n).floor).step(6) do |i|
+		if n % i == 0 or n % ( i + 2 ) == 0
+			return nil
+		end
+	end
+	true
 end
 
-def euler_sieve(list)
+# given a list, returns all primes
+def euler_sieve( list )
     prime = []
     while list != [] do
         new = list.map { |i| list[0] * i }
