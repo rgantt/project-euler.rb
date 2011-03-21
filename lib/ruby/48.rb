1a443c73abd34ca60e6e2760866125f77d37a8a2
Ryan Gantt <ryan@zuttonet.com>
2011-03-21 12:56:47 -0600

    optimized with Enumerable.inject

--- lib/ruby/48.rb
+++ lib/ruby/48.rb
@@ -1,6 +1,2 @@
 # find the last ten digits of the series 1^1 + 2^2 + ... + 1000^1000
-total = 0
-1.upto(1000) do |n|
-	total += n**n
-end
-puts total
+puts (1..1000).inject{|s,n| s+n**n }.to_s[-10,10]
