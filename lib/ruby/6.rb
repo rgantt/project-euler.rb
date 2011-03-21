afb0ce75affc791d504c8150db5d94237daa3822
Ryan Gantt <ryan@zuttonet.com>
2011-03-21 16:34:35 -0600

    used closed-form solution for sum_i^n i^2

--- lib/ruby/6.rb
+++ lib/ruby/6.rb
@@ -1,8 +1,4 @@
 # find the difference between the square of the sums and 
 # the sum of the squares for the first 100 integers
-
-limit, sq = 100, 0
-(1..limit).each do |n|
-    sq += n**2
-end
-puts ((limit+1)*limit/2)**2 - sq
+limit = 100
+puts ((limit+1)*limit/2)**2 - (limit*(limit+1)*(2*limit+1))/6
