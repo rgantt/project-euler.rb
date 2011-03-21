6db9390f5691ef5933d856e96f2087df01549455
Ryan Gantt <ryan@zuttonet.com>
2011-03-21 13:32:15 -0600

    substituted array for accumulator

--- lib/ruby/21.rb
+++ lib/ruby/21.rb
@@ -1,22 +1,16 @@
 # evaluate the sum of all the amicable numbers under 10000.
 require 'common/math'
-
 bound = 7000
-
 sf = [0]
 (1..bound).each do |n|
 	sf << Math.d(n)
 end
-
 # a and b are amicable if d(b) = a and d(a) = b and a != b
-amicable = []
+total = 0
 (1..bound).each do |i|
 	(i..bound).each do |j|
 		next if i == j
-		amicable << [ i, j ] and puts "#{i}, #{j}" if ( i == sf[j] and j == sf[i] )
+		total += i+j and puts "#{i}, #{j}" if ( i == sf[j] and j == sf[i] )
 	end
 end
-total = 0
-amicable.collect{ |a| total += (a.first + a.last) }
 puts total
-#puts nums
