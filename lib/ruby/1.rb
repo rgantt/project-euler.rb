775b44ef2a08e240a829a7989f8dd4b81410b98e
gavinchi <ryan@zuttonet.com>
2011-03-20 21:54:19 -0600

    used inject instead of a silly accumulator

--- lib/ruby/1.rb
+++ lib/ruby/1.rb
@@ -1,3 +1,2 @@
-total = 0
-1000.times{ |n| total += n if ( n%3 == 0 or n%5 == 0 ) }
-puts total
+# find the sum of all multiples of 3 or 5 below 1000
+puts (0..999).to_a.inject{ |sum, x| ( x%3 == 0 or x%5 == 0 ) ? ( sum + x ) : sum }
