2563373bbf07b02a4a6f8a86f9d5291b554cd49b
Ryan Gantt <ryan@zuttonet.com>
2011-04-01 12:41:56 -0600

    english-sounding method name

--- lib/ruby/15.rb
+++ lib/ruby/15.rb
@@ -1,17 +1,17 @@
 # how many routes are there from top-left to bottom-right in a 20x20 grid?
 @_grid = [[]] # filling out the grid for O(1) lookups!
-def val( i, j )
+def routes_to_zero( i, j )
 	return @_grid[i][j] if (@_grid[i] and @_grid[i][j])
 	return 1 if i == 0 or j == 0
 	tot = 0
-	0.upto(j){ |n| tot += val( i - 1, n ) }
+	0.upto(j){ |n| tot += routes_to_zero( i - 1, n ) }
 	@_grid[i] = [] if !(@_grid[i])
 	@_grid[i][j] = tot
 	tot
 end
 
-puts val( 1, 1 ) # better be 2
-puts val( 2, 2 ) # better be 6
-puts val( 3, 3 ) # better be 20
-puts val( 4, 4 ) # better be 70
-puts val( 20, 20 ) # better be 137846528820
+puts routes_to_zero( 1, 1 ) # better be 2
+puts routes_to_zero( 2, 2 ) # better be 6
+puts routes_to_zero( 3, 3 ) # better be 20
+puts routes_to_zero( 4, 4 ) # better be 70
+puts routes_to_zero( 20, 20 ) # better be 137846528820
