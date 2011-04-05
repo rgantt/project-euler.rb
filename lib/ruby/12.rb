c8db86a0bf5157fadef7a238cf7e8e4297f769b5
Ryan Gantt <ryan@zuttonet.com>
2011-04-05 16:23:52 -0600

    clean-up

--- lib/ruby/12.rb
+++ lib/ruby/12.rb
@@ -1,8 +1,8 @@
-# What is the value of the first triangle number to have over five hundred divisors?
+# what is the value of the first triangle number to have over five hundred divisors?
 require File.dirname(__FILE__)+"/common/math"
 
-count, n, triangle = 500, -1, 0
-while triangle+=(n+=1)+1
-	break if Prime.prime_division( triangle, Prime::EratosthenesGenerator.new ).inject(1){ |c,p| c * ( p[1] + 1 ) } > count
+count, n, t = 500, -1, 0
+while t+=(n+=1)+1
+	break if Prime.prime_division( t, Prime::EratosthenesGenerator.new ).inject(1){|c,p| c*(p[1]+1) } > count
 end
 puts triangle
