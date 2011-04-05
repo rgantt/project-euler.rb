fcdbd988b9c12546071937749b7c1c7c93d57f59
gavinchi <ryan@zuttonet.com>
2011-04-04 19:51:32 -0600

    initial solution

--- lib/ruby/30.rb
+++ lib/ruby/30.rb
@@ -1,8 +1,6 @@
 # find the sum of all numbers that can be written as the sum of the fifth powers of their digits
-
-n = 2
-while true
-	break if n.to_s.chars.to_a.inject(0){ |s,n| s + n.to_i**4 } == n
-	n += 1
+n, nums, lim = 1, [], 5*(9**5) # this is the worst upper bound ever
+while ( n += 1 ) < lim
+	nums << n if n.to_s.chars.to_a.inject(0){ |s,n| s + n.to_i**5 } == n
 end
-puts n
+puts nums.inject(:+).to_s
