56fb4970dc79a8a873695c6593716da7033ea3c6
Ryan Gantt <ryan@zuttonet.com>
2011-04-05 12:05:46 -0600

    ELEMENTARY SCHOOL

--- lib/ruby/12.rb
+++ lib/ruby/12.rb
@@ -1,9 +1,8 @@
 # What is the value of the first triangle number to have over five hundred divisors?
-require 'common/math'
-count, n, triangle = 500, 1, 0
-while true
-	triangle = n*(n+1)/2
-	break if Math.factors( triangle ).length > count
-	n += 1
+require File.dirname(__FILE__)+"/common/math"
+
+count, n, triangle = 500, -1, 0
+while triangle+=(n+=1)+1
+	break if Prime.prime_division( triangle, Prime::EratosthenesGenerator.new ).inject(1){ |c,p| c * ( p[1] + 1 ) } > count
 end
 puts triangle
